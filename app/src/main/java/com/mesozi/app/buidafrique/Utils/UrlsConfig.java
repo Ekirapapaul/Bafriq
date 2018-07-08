fa44d1253605bf384eeea4e6fb3ee33835407a41
Paul Ekirapa <ekirapapaul@gmail.com>
2018-07-08 23:21:35 +0300

    [ADD] urls

---
+++ app/src/main/java/com/mesozi/app/buidafrique/Utils/UrlsConfig.java
@@ -0,0 +1,11 @@
+package com.mesozi.app.buidafrique.Utils;
+
+/**
+ * Created by ekirapa on 7/8/18 .
+ */
+public class UrlsConfig {
+    private static final String BASE_IP = "http://portal.buildafrique.com:8089/web/";
+
+    public static final String URL_LOGIN = BASE_IP + "session/authenticate";
+    public static final String URL_DATASET = BASE_IP + "dataset/call_kw";
+}
