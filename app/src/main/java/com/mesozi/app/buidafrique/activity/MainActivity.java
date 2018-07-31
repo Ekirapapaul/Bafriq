3bdb41664790f521463b664ff924286e78f37ed1
Paul Ekirapa <ekirapapaul@gmail.com>
2018-08-01 00:35:06 +0300

    [UPDATE] welcome and home screen changes

--- app/src/main/java/com/mesozi/app/buidafrique/activity/MainActivity.java
+++ app/src/main/java/com/mesozi/app/buidafrique/activity/MainActivity.java
@@ -1,21 +1,32 @@
 package com.mesozi.app.buidafrique.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.mesozi.app.buidafrique.R;
+import com.mesozi.app.buidafrique.Utils.SessionManager;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
 
 /**
- * Created by ekirapa on 7/4/18 .
+ * Home activity class.
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private SearchView searchView;
-    private TextView placeholder;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,24 +36,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void registerViews() {
-        placeholder = findViewById(R.id.tv_search_placeholder);
-        searchView = findViewById(R.id.search_view);
-        //searchView.onActionViewExpanded();
-        searchView.setFocusable(true);// searchView is null
-        searchView.setFocusableInTouchMode(true);
 
-        placeholder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                placeholder.setVisibility(View.GONE);
-                searchView.onActionViewExpanded();
-            }
-        });
+        Toolbar mTopToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mTopToolbar);
+        setDrawer();
 
         findViewById(R.id.menu_customers).setOnClickListener(this);
         findViewById(R.id.menu_leads).setOnClickListener(this);
         findViewById(R.id.menu_sales_orders).setOnClickListener(this);
 
+    }
+
+    private void setDrawer() {
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                logout();
+                break;
+            case R.id.compose:
+                break;
+            case R.id.inbox:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Log out?");
+        builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                new SessionManager(getBaseContext()).setLoggedIn(false);
+                Intent intent = new Intent(getBaseContext(), WelcomeActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        });
+        builder.setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.show();
     }
 
     @Override
@@ -58,5 +117,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(new Intent(getBaseContext(), SalesOrderActivity.class));
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
     }
 }
