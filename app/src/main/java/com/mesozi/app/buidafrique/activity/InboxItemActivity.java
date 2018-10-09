8f7b9e3a57d1cf2f5b0ae9b516fe4f3b8ad6940a
Paul Ekirapa <ekirapapaul@gmail.com>
2018-10-09 15:41:53 +0300

    [UPDATE] few changes

--- app/src/main/java/com/mesozi/app/buidafrique/activity/InboxItemActivity.java
+++ app/src/main/java/com/mesozi/app/buidafrique/activity/InboxItemActivity.java
@@ -3,6 +3,8 @@ package com.mesozi.app.buidafrique.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.mesozi.app.buidafrique.Models.EmailMessage;
@@ -13,7 +15,7 @@ import com.mesozi.app.buidafrique.Utils.CommonUtils;
  * Created by ekirapa on 10/5/18 .
  */
 public class InboxItemActivity extends AppCompatActivity {
-    TextView name, from, toUser, subject, body;
+    TextView name, from, toUser, subject, body, initials;
     EmailMessage message = null;
 
     @Override
@@ -23,6 +25,15 @@ public class InboxItemActivity extends AppCompatActivity {
         message = getIntent().getParcelableExtra("parcel_data");
 
         setViews();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void setViews() {
@@ -31,13 +42,18 @@ public class InboxItemActivity extends AppCompatActivity {
         toUser = findViewById(R.id.tv_to);
         subject = findViewById(R.id.tv_subject);
         body = findViewById(R.id.tv_body);
+        initials = findViewById(R.id.tv_initials);
 
         if (message != null) {
             name.setText(message.getDisplay_name());
             from.setText(String.format("From %s", message.getEmail_from()));
             toUser.setText(String.format("To %s", message.getReply_to()));
-            subject.setText(message.getSubject());
+            subject.setText(message.getSubject().equals("false") ? "No Subject" : message.getSubject());
             body.setText(CommonUtils.fromHtml(message.getBody()));
+            if(message.getEmail_from().length() > 0){
+                char letter = message.getEmail_from().toUpperCase().charAt(0);
+                initials.setText(String.valueOf(letter));
+            }
 
         }
     }
